b77812e5af046ebbaa3212a6aaf755bb16b89a55
M1841 <mihaimuresan592003@gmail.com>
2025-11-08 23:32:38 +0200

    design the input relays

--- simladd/lib/expressions.ts
+++ simladd/lib/expressions.ts
@@ -13,11 +13,13 @@ class ExpressionNode implements Node {
 
   render(): HTMLDivElement {
     const div = document.createElement("div");
-    div.classList.add(this.operator);
+    div.classList.add("expression-node");
 
     this.operands.forEach((operand) => {
       div.appendChild(operand.render());
     });
+    div.classList.add(this.operator);
+
     return div;
   }
 }
@@ -35,23 +37,46 @@ class ConjunctiveNode extends ExpressionNode {
 }
 
 class LiteralNode implements Node {
-  value: string;
+  address: string;
+  label: string;
   isOpen: boolean;
 
-  constructor(value: string, isOpen: boolean = true) {
-    this.value = value;
+  constructor(address: string, label: string, isOpen: boolean = true) {
+    this.address = address;
+    this.label = label;
     this.isOpen = isOpen;
   }
 
   render(): HTMLDivElement {
-    const div = document.createElement("div");
+    const node = document.createElement("div");
+    node.classList.add("literal-node");
 
-    if (!this.isOpen) {
-      div.classList.add("NOT");
-    }
-    div.textContent = this.value;
+    const address = document.createElement("span");
+    address.classList.add("node-address");
+    address.textContent = this.address;
+    address.contentEditable = "true";
+    address.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        address.blur();
+      }
+    });
+    address.addEventListener("blur", () => {
+      this.address = address.textContent;
+      console.log(this);
+    });
+    node.appendChild(address);
 
-    return div;
+    const icon = document.createElement("div");
+    icon.classList.add("node-icon");
+    icon.textContent = `——]${this.isOpen ? " " : "/"}[——`;
+    node.appendChild(icon);
+
+    const label = document.createElement("span");
+    label.classList.add("node-label");
+    label.textContent = `"${this.label}"`;
+    node.appendChild(label);
+
+    return node;
   }
 }
 
@@ -61,27 +86,28 @@ enum Operator {
 }
 
 function renderExample() {
-  const literalTree = new LiteralNode("START");
+  const literalTree = new LiteralNode("%I0.0", "START");
   const conjunctiveTree = new ConjunctiveNode([
-    new LiteralNode("START"),
-    new LiteralNode("STOP", false),
+    new LiteralNode("%I0.0", "START"),
+    new LiteralNode("%I0.1", "STOP", false),
   ]);
   const disjunctiveTree = new DisjunctiveNode([
-    new LiteralNode("START"),
-    new LiteralNode("LED"),
+    new LiteralNode("%I0.0", "START"),
+    new LiteralNode("%Q0.0", "LED"),
   ]);
   const nestedTree = new DisjunctiveNode([
     new ConjunctiveNode([
-      new LiteralNode("START"),
-      new LiteralNode("STOP", false),
+      new LiteralNode("%I0.0", "START"),
+      new LiteralNode("%I0.1", "STOP", false),
     ]),
-    new LiteralNode("LED"),
+    new LiteralNode("%Q0.0", "LED"),
   ]);
 
   const sample = document.createElement("div");
   sample.style.display = "flex";
   sample.style.flexDirection = "column";
-  sample.style.gap = "60px";
+  sample.style.gap = "30px";
+  sample.style.padding = "20px";
 
   sample.appendChild(literalTree.render());
   sample.appendChild(conjunctiveTree.render());
