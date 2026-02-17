cde89494ba5e3015deee7a910ea9ef041e1903c0
Mihai Mureșan <mihaimuresan592003@gmail.com>
2026-02-17 15:31:39 +0200

    add a few more menu buttons

--- lib/menu/Menu.ts
+++ lib/menu/Menu.ts
@@ -1,56 +1,97 @@
-import { Menu, MenuItem, Submenu } from "@tauri-apps/api/menu";
+import {
+  Menu,
+  MenuItem,
+  PredefinedMenuItem,
+  Submenu,
+} from "@tauri-apps/api/menu";
 import { appDataDir } from "@tauri-apps/api/path";
 import { open, save } from "@tauri-apps/plugin-dialog";
+import { exit } from "@tauri-apps/plugin-process";
+import { load } from "@tauri-apps/plugin-store";
 
-const fileSubmenu = await Submenu.new({
-  text: "File",
-  items: [
-    await MenuItem.new({
-      id: "new",
-      text: "New",
-      action: () => {},
-    }),
-    await MenuItem.new({
-      id: "open",
-      text: "Open",
-      action: async () => {
-        const path = await open({
-          title: "Open a project",
-          multiple: false,
-          directory: false,
-          filters: [
-            {
-              name: "JSON",
-              extensions: ["json"],
-            },
-          ],
-          defaultPath: await appDataDir(),
-        });
-        console.log(path);
-      },
-    }),
-    await MenuItem.new({
-      id: "save",
-      text: "Save",
-      action: async () => {
-        const path = await save({
-          title: "Save project",
-          filters: [
-            {
-              name: "JSON",
-              extensions: ["json"],
-            },
-          ],
-          defaultPath: await appDataDir(),
-        });
-        console.log(path);
-      },
-    }),
-  ],
-});
+const store = await load("store.json");
 
 const menu = await Menu.new({
-  items: [fileSubmenu],
+  items: [
+    await Submenu.new({
+      text: "File",
+      items: [
+        await MenuItem.new({
+          id: "new",
+          text: "New",
+          action: () => {},
+        }),
+        await MenuItem.new({
+          id: "open",
+          text: "Open",
+          action: async () => {
+            const path = await open({
+              title: "Open a project",
+              multiple: false,
+              directory: false,
+              filters: [
+                {
+                  name: "JSON",
+                  extensions: ["json"],
+                },
+              ],
+              defaultPath: await appDataDir(),
+            });
+            await store.set("project-path", { value: path });
+          },
+        }),
+        await PredefinedMenuItem.new({
+          text: "separator",
+          item: "Separator",
+        }),
+        await MenuItem.new({
+          id: "save",
+          text: "Save",
+          action: () => {},
+        }),
+        await MenuItem.new({
+          id: "save-as",
+          text: "Save as",
+          action: async () => {
+            const path = await save({
+              title: "Save project as",
+              filters: [
+                {
+                  name: "JSON",
+                  extensions: ["json"],
+                },
+              ],
+              defaultPath: await appDataDir(),
+            });
+            await store.set("project-path", { value: path });
+          },
+        }),
+        await PredefinedMenuItem.new({
+          text: "separator",
+          item: "Separator",
+        }),
+        await MenuItem.new({
+          id: "exit",
+          text: "Exit",
+          action: async () => {
+            await exit(0);
+          },
+        }),
+      ],
+    }),
+    await Submenu.new({
+      text: "Edit",
+      items: [],
+    }),
+    await Submenu.new({
+      text: "View",
+      items: [],
+    }),
+    await Submenu.new({
+      text: "Help",
+      items: [],
+    }),
+  ],
 });
 
 export { menu as Menu };
